206968141f2a592ed1c21b24a35738c1f896653c
Kshitinjay <kshitinjay20@gmail.com>
2021-02-18 02:09:59 +0530

    easy one

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,59 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import slides from "../data";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <></>
-  )
-}
+  const [click, setClick] = useState(0);
+  const [disabledNext, setDisabledNext] = useState(false);
+  const [disabledPrev, setDisabledPrev] = useState(true);
 
+  const handleNext = () => {
+    setDisabledPrev(false);
+    if (click < 4) {
+      setClick(click + 1);
+      if (click == 3) {
+        setDisabledNext(true);
+      }
+    }
+  };
+  const handlePrev = () => {
+    setDisabledNext(false);
+    if (click > 0) {
+      setClick(click - 1);
+      if (click == 1) {
+        setDisabledPrev(true);
+      }
+    }
+  };
+
+  const handleReset = () => {
+    setClick(0);
+    setDisabledPrev(true);
+    setDisabledNext(false);
+  };
+  return (
+    <>
+      <h1 data-testid="title">{slides[click].title}</h1>
+      <p data-testid="text">{slides[click].text}</p>
+      <button
+        onClick={handlePrev}
+        data-testid="button-prev"
+        disabled={disabledPrev}
+      >
+        Prev
+      </button>
+      <button
+        onClick={handleNext}
+        data-testid="button-next"
+        disabled={disabledNext}
+      >
+        Next
+      </button>
+      <button onClick={handleReset} data-testid="button-reset">
+        Reset
+      </button>
+    </>
+  );
+};
 
 export default App;
