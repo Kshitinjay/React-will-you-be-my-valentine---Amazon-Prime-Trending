77fc0ed7df381f0d05254e9d0eb6dcd9061efb5a
Kshitinjay <kshitinjay20@gmail.com>
2021-02-20 21:20:10 +0530

    checking

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,16 @@
-import React, { useState } from "react";
-import slides from "../data";
-import "../styles/App.css";
+import React from "react";
+import '../styles/App.css';
+import Container from "./Container";
 
-const App = () => {
-  const [click, setClick] = useState(0);
-  const [disabledNext, setDisabledNext] = useState(false);
-  const [disabledPrev, setDisabledPrev] = useState(true);
-  const [disabledReset, setDisabledReset] = useState(true);
-
-  const handleNext = () => {
-    setDisabledPrev(false);
-    setDisabledReset(false);
-    if (click < 4) {
-      setClick(click + 1);
-      if (click == 3) {
-        setDisabledNext(true);
-      }
-    }
-  };
-  const handlePrev = () => {
-    setDisabledNext(false);
-    if (click > 0) {
-      setClick(click - 1);
-      if (click == 1) {
-        setDisabledPrev(true);
-      }
-    }
-  };
-
-  const handleReset = () => {
-    setClick(0);
-    setDisabledPrev(true);
-    setDisabledNext(false);
-    setDisabledReset(true);
-  };
+const App = ({slides}) => {
   return (
     <>
-      <div id="navigation">
-        <div id="slide">
-          <h1 data-testid="title">{slides[click].title}</h1>
-          <p data-testid="text">{slides[click].text}</p>
-        </div>
-
-        <button
-          onClick={handlePrev}
-          data-testid="button-prev"
-          disabled={disabledPrev}
-        >
-          Prev
-        </button>
-        <button
-          onClick={handleNext}
-          data-testid="button-next"
-          disabled={disabledNext}
-        >
-          Next
-        </button>
-        <button
-          onClick={handleReset}
-          data-testid="button-restart"
-          disabled={disabledReset}
-        >
-          Reset
-        </button>
-      </div>
+    <div className="App">
+    <Container slides={slides}/>
+    </div>
     </>
-  );
-};
+  )
+}
+
 
 export default App;
